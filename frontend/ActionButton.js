8debb86ed1a3cbeb0fc090bb68f3fec5ba4cfb35
Lee Robinson <lrobinson2011@gmail.com>
2020-06-28 17:01:02 -0500

    Add prettier, flexbox, better styling.

--- frontend/ActionButton.js
+++ frontend/ActionButton.js
@@ -1,26 +1,23 @@
 import React from 'react';
-import { Icon } from '@airtable/blocks/ui';
+import { Button } from '@airtable/blocks/ui';
 
-function ActionButton({ actionText, iconName, record, clickAction }) {
+function ActionButton({ actionText, iconName, record, clickAction, ...rest }) {
   return (
-    <div style={{ width: '20%', display: 'inline-block', textAlign: 'center' }}>
-      <a
-        style={{
-          cursor: 'pointer',
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center'
-        }}
-        onClick={() => {
-          clickAction(record);
-        }}
-      >
-        <Icon name={iconName} size={16} />
-        <div
-          style={{ display: 'inline-block', paddingLeft: 3 }}
-        >{` ${actionText}`}</div>
-      </a>
-    </div>
+    <Button
+      variant="secondary"
+      size="small"
+      style={{
+        transition: '0.15s all ease',
+        padding: 0
+      }}
+      icon={iconName}
+      onClick={() => {
+        clickAction(record);
+      }}
+      {...rest}
+    >
+      {actionText}
+    </Button>
   );
 }
 
--- frontend/Reminder.js
+++ frontend/Reminder.js
@@ -1,17 +1,22 @@
 import React from 'react';
-import { expandRecord, CellRenderer, Icon } from '@airtable/blocks/ui';
+import {
+  expandRecord,
+  CellRenderer,
+  Box,
+  Text,
+  Icon
+} from '@airtable/blocks/ui';
 import { sendReminder } from './utils/sendReminder';
 import ActionButton from './ActionButton';
 import { getDueDate } from './utils/getDueDate';
 
-// TODO: Figure out stylesheet + hover
-// background-color: rgba(0,0,0,0.05);
-// transition: .085s background-color ease-in;
 function Reminder({ record, config, table }) {
   const ownerField =
     config.get('ownerField') && table.getFieldById(config.get('ownerField'))
       ? table.getFieldById(config.get('ownerField'))
       : null;
+
+  const subjectField = config.get('subjectField');
 
   return (
     <div
@@ -22,51 +27,44 @@ function Reminder({ record, config, table }) {
         fontWeight: 600
       }}
     >
-      <div style={{ width: '60%', display: 'inline-block', fontSize: '.9rem' }}>
-        {/* MOVE TO ONLY RENDER CONDITIONALLY */}
-        {/* <CellRenderer
-          field={statusField}
-          record={record}
-          style={{ display: 'inline-block', marginRight: 5 }}
-        /> */}
-        {/* <Icon name="checkboxUnchecked" size={16} /> */}
-        {config.get('subjectField')
-          ? record.getCellValueAsString(config.get('subjectField')) ||
-            '(Untitled)'
-          : record.primaryCellValueAsString || '(Untitled)'}
-      </div>
-
-      <ActionButton
-        actionText="Remind"
-        iconName="bell"
-        record={record}
-        clickAction={() => sendReminder(record, config)}
-      />
-
-      <ActionButton
-        actionText="Edit"
-        iconName="edit"
-        record={record}
-        clickAction={expandRecord}
-      />
-      <div style={{ display: 'inline-block', width: '100%', paddingTop: 5 }}>
-        {/* Summary */}
-        <div
-          style={{
-            width: '60%',
-            display: 'inline-block',
-            fontSize: '.7rem',
-            color: '#666',
-            fontWeight: 400,
-            float: 'left'
-          }}
-        >
+      <Box display="flex" justifyContent="space-between" alignItems="center">
+        <Text size="large" fontWeight="600">
+          {/* <Icon name="checkboxUnchecked" size={16} /> */}
+          {subjectField
+            ? record.getCellValueAsString(subjectField) || '(Untitled)'
+            : record.primaryCellValueAsString || '(Untitled)'}
+        </Text>
+        <Box>
+          <ActionButton
+            actionText="Remind"
+            iconName="bell"
+            record={record}
+            clickAction={() => sendReminder(record, config)}
+            marginRight={2}
+          />
+          <ActionButton
+            actionText="Edit"
+            iconName="edit"
+            record={record}
+            clickAction={expandRecord}
+          />
+        </Box>
+      </Box>
+      <Box
+        display="flex"
+        justifyContent="space-between"
+        alignItems="flex-start"
+        paddingTop={1}
+      >
+        <Box>
           {config.get('ownerField') ? (
             record.getCellValueAsString(config.get('ownerField')) ? (
               <CellRenderer
                 field={ownerField}
                 record={record}
-                style={{ display: 'inline-block', marginRight: 5 }}
+                cellStyle={{
+                  'margin-left': '-6px'
+                }}
               />
             ) : (
               'Unassigned'
@@ -74,28 +72,16 @@ function Reminder({ record, config, table }) {
           ) : (
             ' '
           )}
-          {config.get('summaryField') ? (
-            <div style={{ width: '100%', fontStyle: 'italic' }}>
-              {record.getCellValueAsString(config.get('summaryField'))}
-            </div>
-          ) : (
-            ' '
-          )}
-        </div>
-
-        {/* Due in X Days */}
-        <div
-          style={{
-            width: '40%',
-            display: 'inline-block',
-            fontSize: '.7rem',
-            textAlign: 'center',
-            float: 'right'
-          }}
-        >
+          <Text fontStyle="italic" textColor="light" marginTop={2}>
+            {config.get('summaryField')
+              ? record.getCellValueAsString(config.get('summaryField'))
+              : ' '}
+          </Text>
+        </Box>
+        <Text fontWeight="600" minWidth="110px">
           {config.get('dueDateField') ? getDueDate(config, table, record) : ' '}
-        </div>
-      </div>
+        </Text>
+      </Box>
     </div>
   );
 }
